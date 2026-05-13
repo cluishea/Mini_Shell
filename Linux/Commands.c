4c4cbdcc1eaea39ca31205bb46616124b6510a2a
cluishea <artifexcluishe@gmail.com>
2026-05-13 16:39:59 +0545

    "Added

--- Linux/Commands.c
+++ Linux/Commands.c
@@ -21,7 +21,6 @@ _CMD_ERR RunPOSIXCommand(char **command){
         else{
             exit(0);
         }
-        
     }
     else{
         int exitCode;
@@ -41,6 +40,25 @@ _CMD_ERR Run(char **command){
         return 1;
         }  
     }
+    else if(strcmp(command[0],"cd")==0){
+        if(command[1]==NULL){
+            fprintf(stderr,"'cd' takes an argument but no arguments were given.\n");
+            return 1;
+        }
+        else if (command[2]!=NULL){
+            fprintf(stderr,"'cd' takes an argument but more arguments were given.\n");
+            return 1;
+        }
+        else{
+                if(chdir(command[1]) != 0){ 
+                    fprintf(stderr,"Could not change directory to %s.\n",command[1]);
+                    return 1;
+                }
+                else{
+                    return 0;
+                }
+        } 
+    }
     else{
         return RunPOSIXCommand(command);
     }
@@ -48,13 +66,27 @@ _CMD_ERR Run(char **command){
 
 
 _CMD_ERR ExecuteCommand(char** command){
-    
+    int commandExecuted = 0;
     char *currentCommand[PARSER_NUM_WORDS];
-
-    Run(command);
 
     int j=0;
     for(int i = 0;command[i]!=NULL;i++){
+        if (strcmp(command[i],"&&")==0){
+            currentCommand[j] = NULL;
+            commandExecuted=1;
+            if(Run(currentCommand)==1){
+                break;
+            }
+            j=-1;
+        }else{
+            commandExecuted=0;
+            currentCommand[j] = command[i];
+        }
+        j++;
+    }
+    if(!commandExecuted){
+        currentCommand[j]=NULL;
+        Run(currentCommand);
     }
 
 }
